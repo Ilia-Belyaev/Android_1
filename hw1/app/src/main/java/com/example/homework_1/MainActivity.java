d43afd5b83bb049388b5e868f4d0b50dc75e5b2d
Ilia Belyaev <ilya.belyaev.95@list.ru>
2021-05-16 11:34:30 +0300

    Домашняя работа 1. Пояснения: 1. Скриншоты работающих приложений прикрепил. 2. В первом приложении с помощью кнопки меняются местами записи типо hello world и тем, что напишет пользователь в EditText. Switch меняет цвет при переключении и меняет текст в CheckBox. Ну и календарь пихнул вниз. ToggleButton меняет цвет фона, текст и цвет текста самого себя. 3. Во втором приложении пользователь вводит в поля значения и по нажатию кнопки введённые параметры отбражаются в TextView под кнопкой.

--- hw1/app/src/main/java/com/example/homework_1/MainActivity.java
+++ hw1/app/src/main/java/com/example/homework_1/MainActivity.java
@@ -2,13 +2,81 @@ package com.example.homework_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private EditText editText;
+    private TextView textView;
+    private Editable emptyString;
+    private String emptyString2;
+    private Switch aSwitch;
+    private CheckBox checkBox;
+    private ToggleButton toggleButton;
+    private LinearLayout linearLayout;
+    private CalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emptyString = editText.getText();
+                editText.setText(textView.getText());
+                textView.setText(emptyString);
+            }
+        });
+        checkBox = findViewById(R.id.checkbox);
+        aSwitch = findViewById(R.id.switch1);
+        emptyString2 = (String) checkBox.getHint();
+        aSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (aSwitch.isChecked()) {
+                    aSwitch.setBackgroundColor(5);
+                    checkBox.setText("Забыть помыть посуду и получить люлей");
+                    checkBox.setChecked(true);
+                } else {
+                    checkBox.setText(emptyString2);
+                    checkBox.setChecked(false);
+                }
+
+            }
+        });
+        toggleButton = (ToggleButton) findViewById(R.id.toggleButton2);
+        calendarView = findViewById(R.id.calendar);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(toggleButton.isChecked()){
+                    toggleButton.setBackgroundColor(Color.BLACK);
+                    toggleButton.setTextColor(Color.WHITE);
+
+                }else {
+                    toggleButton.setBackgroundColor(Color.WHITE);
+                    toggleButton.setTextColor(Color.BLACK);
+
+                }
+            }
+        });
+
+
     }
+
+
 }
--- hw1/app/src/main/java/com/example/homework_1/MainActivity2.java
+++ hw1/app/src/main/java/com/example/homework_1/MainActivity2.java
@@ -3,12 +3,39 @@ package com.example.homework_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private EditText something,name,phone,number,pass,mail;
+    private Button button;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        something = findViewById(R.id.editText);
+        name = findViewById(R.id.editTextPersonName);
+        phone = findViewById(R.id.editPhone);
+        number = findViewById(R.id.editNumber);
+        pass = findViewById(R.id.editPassword);
+        mail = findViewById(R.id.editEmail);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Я всё про тебя знаю:" + "\n"
+                        +   "Случайный текст: " + something.getText() +"\n"
+                        +   "Ваше имя: "+name.getText() + "\n"
+                        +   "Ваш телефон: " + phone.getText() + "\n"
+                        +   "Ваше число: " + number.getText() + "\n"
+                        +   "Ваш пароль: " + pass.getText() + "\n"
+                        +   "Ваш Email: " + mail.getText() + "\n");
+            }
+        });
     }
 }
