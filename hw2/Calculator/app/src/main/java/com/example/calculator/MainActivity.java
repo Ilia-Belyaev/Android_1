5a2b216bc0152dd2143ba0d58dea7edaaf76f48e
Ilia Belyaev <ilya.belyaev.95@list.ru>
2021-05-19 20:12:50 +0300

    Пустое приложение дз номер 2.

---
+++ hw2/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//<androidx.appcompat.widget.AppCompatImageView
+//        android:id="@+id/imageView1"
+//        android:layout_width="match_parent"
+//        android:layout_height="wrap_content"
+//        app:srcCompat="@drawable/photo" />
